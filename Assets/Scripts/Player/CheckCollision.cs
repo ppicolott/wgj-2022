c8b29080d10e36aedd71f5838195ce4225094b60
Paula Picolott <paula_picolott@hotmail.com>
2022-08-21 12:53:00 -0300

    Timer para os danos

--- Assets/Scripts/Player/CheckCollision.cs
+++ Assets/Scripts/Player/CheckCollision.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class CheckCollision : MonoBehaviour
 {
+    float timer = 0.05f;
+    private bool collided = false;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.gameObject.name.Contains("Duck"))
         {
-            Duck.life -= 25;
+            collided = true;
+            if(timer <= 0)
+            {
+                Duck.life -= 25;
+                timer = 2f;
+                collided = false;
+            }
         }
+    }
+
+    private void Update()
+    {
+        if(collided)
+        {
+            timer -= Time.deltaTime;
+        }
+        Debug.Log(Duck.life);
     }
 }
